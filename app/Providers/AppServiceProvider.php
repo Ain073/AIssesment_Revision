7f5a31343c57de91d0273e271015528acdb01114
John Nythan Carl F. Pidlaoan <nythancarl@gmail.com>
2026-08-13 04:17:28 +0800

    Revert "Force HTTPS URLs in production"
    This reverts commit 795e1180c1e4822fe85a4809d38081cb25029c3e.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ use App\Models\User;
 use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,10 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
-
         RedirectIfAuthenticated::redirectUsing(function (Request $request): string {
             /** @var User|null $user */
             $user = Auth::user();
